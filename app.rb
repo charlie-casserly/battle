9ea2c68ba89a416ec2002a1fa77a688811e1b82d
charlie-casserly <charliecasserly215@gmail.com>
2021-05-14 11:26:18 +0100

    Test helpers added

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class Battle < Sinatra::Base
   post '/names' do
     session[:player_1_name] = params[:player_1_name]
     session[:player_2_name] = params[:player_2_name]
-    redirect :play
+    redirect '/play'
   end
 
   get '/play' do
